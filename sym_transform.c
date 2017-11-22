f76547a4fd7f6b2f18706f3ac61f8b7ca936a7ba
Max Gautier <mgautier@student.42.fr>
2017-11-22 19:30:56 +0100

    Update sym_transform to uses true "empty prod"
    Also updates test.

--- sym_transform.c
+++ sym_transform.c
@@ -6,11 +6,12 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 16:34:55 by mgautier          #+#    #+#             */
-/*   Updated: 2017/11/20 09:58:28 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/11/21 18:31:45 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sym_defs.h"
+#include "prods_interface.h"
 #include "libft.h"
 #include <stdlib.h>
 #include <stdarg.h>
@@ -46,10 +47,16 @@ static t_bool	remove_left_recur(void *prod, va_list args)
 	return (append_to_prod(prod, va_arg(args, t_symbol const*)));
 }
 
+/*
+** TODO: Find a way to distinguish between the case where the sym is not left
+** recursive and the one where malloc fails.
+*/
+
 t_symbol		*eliminate_left_recursion(t_symbol *left_recursive_sym)
 {
 	t_lst		*left_rec_prods;
 	t_symbol	*new_sym;
+	t_prod		*empty_prod;
 
 	left_rec_prods = take_left_recursive_prods(left_recursive_sym);
 	if (left_rec_prods != NULL)
@@ -60,7 +67,11 @@ t_symbol		*eliminate_left_recursion(t_symbol *left_recursive_sym)
 			f_lstiterr_va(left_recursive_sym->prods, gen_append_to_prod, new_sym);
 			new_sym->prods = left_rec_prods;
 			f_lstiterr_va(new_sym->prods, remove_left_recur, new_sym);
-			add_prod(new_sym, get_empty_prod());
+			empty_prod = create_prod();
+			if (empty_prod != NULL)
+				add_prod(new_sym, empty_prod);
+			else
+				destroy_symbol(&new_sym);
 		}
 	}
 	else
