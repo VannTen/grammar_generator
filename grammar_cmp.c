cab9ccbcbca6ea62c32cc74f82b9db29a2e50ba1
Max Gautier <mgautier@student.42.fr>
2017-12-20 19:18:21 +0100

    Adjust grammar comparison : sym's order irrelevant

--- grammar_cmp.c
+++ grammar_cmp.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 11:55:08 by mgautier          #+#    #+#             */
-/*   Updated: 2017/11/28 13:00:25 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/12/20 19:13:32 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ static t_bool	equ(void const *sym_1, void const *sym_2)
 
 t_bool			gram_are_equ(t_grammar const *gram_1, t_grammar const *gram_2)
 {
-	return (fifo_are_equ(gram_1->sym_list, gram_2->sym_list, equ));
+	return (fifo_same_contents(gram_1->sym_list, gram_2->sym_list, equ));
 }
